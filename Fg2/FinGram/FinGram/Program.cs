cf99430da5699f1484e90df4bf693b0613f459e1
Семён Товстолужский <russianpartyofgamers@gmail.com>
2021-03-20 18:09:37 +0300

    Добавил проект и так же сделал первые наброски.

---
+++ Fg2/FinGram/FinGram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FinGram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Вы зарегистрировались?");
+            Console.WriteLine("Ответ:");
+            string answer = Console.ReadLine();
+            if (answer == "да" || answer == "ДА" || answer == "Да")
+            {
+                Console.WriteLine("Введите логин:");
+                string answerLogin = Console.ReadLine();
+                Console.WriteLine("Введите пароль:");
+                string answerPassword = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
